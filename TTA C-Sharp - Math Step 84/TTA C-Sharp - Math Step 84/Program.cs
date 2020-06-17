1ee549e23854c340d0d3681e7ea310aec610bd71
dsalem <daisymallari1111@gmail.com>
2020-06-17 15:30:53 -0700

    TTA Console Assignment - Step 88

---
+++ TTA C-Sharp - Math Step 84/TTA C-Sharp - Math Step 84/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TTA_C_Sharp___Math_Step_84
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+
+            //int difference = 10 - 5;
+            //Console.WriteLine(difference);
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //int quotient = 10 / 2;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //int remainder = 10 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //int num1 = 200 / 3;
+            //Console.WriteLine(num1);
+            //Console.ReadLine();
+
+            bool trueOrFalse = 12 < 5;
+            Console.Write(trueOrFalse.ToString());
+            Console.ReadLine();
+
+            int roomTemperature = 70;
+            int currentTemperature = 72;
+
+            bool isWarm = currentTemperature >= roomTemperature;
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
+
+             
+
+
+        }
+    }
+}
---
+++ TTA Console Assignment - C-Sharp Step 88/TTA Console Assignment - C-Sharp Step 88/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TTA_Console_Assignment___C_Sharp_Step_88
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number");
+            int yourNumber = Convert.ToInt32(Console.ReadLine());
+            int product = yourNumber * 50;
+            Console.WriteLine(product);
+
+            Console.WriteLine("Pick a number");
+            int yourNumber2 = Convert.ToInt32(Console.ReadLine());
+            int sum1 = yourNumber2 + 25;
+            Console.WriteLine(sum1);
+
+            Console.WriteLine("Pick a number");
+            int yourNumber3 = Convert.ToInt32(Console.ReadLine());
+            double divide1 = yourNumber3 / 12.5;
+            Console.WriteLine(divide1);
+
+            Console.WriteLine("Pick a number");
+            int yourNumber4 = Convert.ToInt32(Console.ReadLine());
+            bool reply1 = yourNumber4 > 50;
+            Console.Write(reply1.ToString());
+       
+            Console.WriteLine("Pick a number");
+            int yourNumber5 = Convert.ToInt32(Console.ReadLine());
+            int remainder = yourNumber5 % 7;
+            Console.WriteLine(remainder);
+
+   
+
+        }
+    }
+}
