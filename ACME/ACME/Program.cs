f198a68ae2d36f0f04867bd4570eb846ddc07a3a
daisysalem <62805119+daisysalem@users.noreply.github.com>
2020-06-17 16:34:02 -0700

    ACME Step 54

---
+++ ACME/ACME/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ACME
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.ReadLine();
+      
+        }
+    }
+}
