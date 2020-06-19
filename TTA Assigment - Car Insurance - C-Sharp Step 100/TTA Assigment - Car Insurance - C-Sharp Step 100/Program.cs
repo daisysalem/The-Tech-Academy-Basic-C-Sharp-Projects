43e55a6a1fed7fd8e625090d9b358ec4581dea46
daisysalem <62805119+daisysalem@users.noreply.github.com>
2020-06-18 18:20:36 -0700

    TTA Assignment - Car Insurance - Step 100

---
+++ TTA Assigment - Car Insurance - C-Sharp Step 100/TTA Assigment - Car Insurance - C-Sharp Step 100/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TTA_Assigment___Car_Insurance___C_Sharp_Step_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car Insurance Evaluation");
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? Answer Yes or No.");
+            string dui = Console.ReadLine();
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualified for Insurance?");
+
+            bool qualify = (age >= 15 && (dui == "no" || dui == "No") && tickets <= 3);
+
+            Console.WriteLine(qualify);
+
+           
+        }
+    }
+}
