9ef14c585f44c789ef8429b77259a97e520fdeb8
daisysalem <62805119+daisysalem@users.noreply.github.com>
2020-06-19 20:10:22 -0700

    CS - Step 121

---
+++ CS - While-Do - Step 121/CS - While-Do - Step 121/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CS___While_Do___Step_121
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess what number I'm thinking of between 1-100");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 9;
+
+            do
+            {
+                switch (number)
+                {
+                    case 80:
+                        Console.WriteLine("Way too high. Guess again");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 40:
+                        Console.WriteLine("Too high. Guess again");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 5:
+                        Console.WriteLine("Too low but close. Guess again");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 9:
+                        Console.WriteLine("Yes, number 9! You are correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("Nope. Guess again");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+            }
+            while (!isGuessed); 
+
+            Console.Read();
+        }
+    }
+}
