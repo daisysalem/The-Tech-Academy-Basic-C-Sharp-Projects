0e94ba6b37f17f13c59caedf4106a1a7b423871b
daisysalem <62805119+daisysalem@users.noreply.github.com>
2020-06-23 15:30:17 -0700

    CS - Class and 3 methods - Step 161

---
+++ CS - Class and Methods - Step 161/CS - Class and Methods - Step 161/MathOP.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS___Class_and_Methods___Step_161
+{
+    class MathOP
+    {
+        public static int Add (int num1)
+        {
+            return num1 + 100;
+        }
+        public static int Mult (int num1)
+        {
+            return num1 * 50;
+        }
+        public static int Subtract (int num1)
+        {
+            return num1 - 10;
+        }
+    }
+}
+
+
+//int Add1 = num1 + 100;
+//int Mult1 = num1 * 50;
+//int Subtract1 = num1 - 10;
+
+//Console.WriteLine("Here are your answers:");
+//            Console.WriteLine(num1 + " + 100 = " + Add1);
+//            Console.WriteLine(num1 + " x 50 = " + Mult1);
+//            Console.WriteLine(num1 + " - 10 = " + Subtract1);
+//            Console.ReadLine();
---
+++ CS - Class and Methods - Step 161/CS - Class and Methods - Step 161/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+1. Create a class. In that class, create three methods, each of which will take one 
+integer parameter in and return an integer. The methods should do some math operation 
+on the received parameter. Put this class in a separate .cs file in the application.
+
+2. In the Main() program, ask the user what number they want to do the math operations on.
+
+3. Call each method in turn, passing the user input to the method. Display the returned integer to the screen. 
+*/
+
+
+using CS___Class_and_Methods___Step_161;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a number and I'll perform 3 math functions with it");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 + " + 100 = " + MathOP.Add(num1));
+            Console.WriteLine(num1 + " x 50 = " + MathOP.Mult(num1));
+            Console.WriteLine(num1 + " - 10 = " + MathOP.Subtract(num1));
+            Console.ReadLine();
+
+        }
+    }
+}
+
