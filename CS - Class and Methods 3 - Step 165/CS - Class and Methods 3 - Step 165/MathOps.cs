2bdc70d6eab3a5aa36d7eec67f71d6db2c601382
daisysalem <62805119+daisysalem@users.noreply.github.com>
2020-06-24 12:37:45 -0700

    CS - Class and Methods 3 - Step 165

---
+++ CS - Class and Methods 3 - Step 165/CS - Class and Methods 3 - Step 165/MathOps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS___Class_and_Methods_3___Step_165
+{
+    public class MathOps
+    {
+        public int MathMethod(int num1, string ans)
+        {
+            int result = 0;
+            if (ans == "")
+            {
+                result = num1 + 10;
+
+            }
+            else
+            {
+                int num2 = Convert.ToInt32(ans);
+                result = num1 + num2 + 10;
+            }
+            return result;
+
+        }
+        
+    }
+}
---
+++ CS - Class and Methods 3 - Step 165/CS - Class and Methods 3 - Step 165/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ 1. Create a class. In that class, create a method that takes two integers as parameters. 
+Make one of them optional. Have the method do a math operation and return an integer result.
+
+2. In the Main() method of the console app, instantiate the class.
+
+3. Ask the user to input two numbers, one at a time. Let them know they need not enter 
+anything for the second number.
+
+4. Call the method in the class, passing in the one or two numbers entered.
+
+5. Try various combinations of numbers on the code, including having no second number.
+*/
+
+using CS___Class_and_Methods_3___Step_165;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick a second number if you want. Otherwise just hit return.");
+            string ans = Console.ReadLine();
+
+            MathOps FirstOp = new MathOps();
+      
+            Console.WriteLine("Your input(s) + 10 = " + FirstOp.MathMethod(num1, ans));
+ 
+            Console.ReadLine();
+
+        }
+    }
+}
+
+            
+
