0a6e080bbaf07d84580b947a4db6cc7642711134
daisysalem <62805119+daisysalem@users.noreply.github.com>
2020-06-24 15:14:19 -0700

    CS - Class and Methods 4 - Step 166

---
+++ CS - Class and Methods 4 - Step 166/CS - Class and Methods 4 - Step 166/MathOp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS___Class_and_Methods_4___Step_166
+{
+    public class MathOp
+    {
+        public void MathMethod(int num1, int num2)
+        {
+            int result = num1 + 100;
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ CS - Class and Methods 4 - Step 166/CS - Class and Methods 4 - Step 166/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+1. Create a class. In that class, create a void method that takes two integers as parameters. 
+Have the method do a math operation on the first integer and display the second integer to the screen.
+
+2. In the Main() method of the console app, instantiate the class.
+
+3. Call the method in the class, passing in two numbers.
+
+4. Call the method in the class, specifying the parameters by name.
+*/
+ 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS___Class_and_Methods_4___Step_166
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick a second number.");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            MathOp FirstOp = new MathOp();
+            FirstOp.MathMethod(num1, num2);
+            
+            Console.ReadLine();
+        }
+    }
+}
