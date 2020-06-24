b0744a2fe303733661674fcb5a92d050a3f45f4d
daisysalem <62805119+daisysalem@users.noreply.github.com>
2020-06-23 18:39:26 -0700

    For Tanner to check

---
+++ CS - Class and Methods 2 - Step 164/CS - Class and Methods 2 - Step 164/MathOp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS___Class_and_Methods_2___Step_164
+{
+    class MathOp
+    {
+        public static int MathMethod(int num1)
+        {
+            int num1Result = num1 + 10;
+            return num1Result;
+        }
+       
+        public static decimal MathMethod(int num1, decimal num2)
+        {
+            decimal num2Result = num2 * 10;
+            return num2Result;
+        }
+
+        public static string MathMethod(int num1, decimal num2, string num3)
+        {
+            int num4 = Convert.ToInt32(num3);
+            int num3Result = num4 - 10;
+            return num3Result;
+        }
+    }
+}
+
+
+
+
+
---
+++ CS - Class and Methods 2 - Step 164/CS - Class and Methods 2 - Step 164/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+ 1. Create a class. In that class, create a method that will take in an integer, 
+create a math operation for this integer (addition, subtraction, etc.), then return 
+the answer as an integer.
+
+2. In the Main() method of the console app, instantiate the class and call the 
+one method, passing in an integer. Display the result to the screen.
+
+3. Add a second method to the class with the same name that will take in a decimal,
+create a different math operation for it, then return the answer as an integer.
+
+4. In the Main() method of the console app, instantiate the class and call the second method, 
+passing in a decimal. Display the result to the screen.
+
+5. Add a third method to the class, with the same name, that will take in a string, 
+convert it to an integer if possible, do a different math operation to it, then return 
+the answer as an integer.
+
+6. In the Main() method of the console app, instantiate the class and call the third method, 
+passing in a string that equates to an integer. Display the result to the screen.
+*/
+
+using CS___Class_and_Methods_2___Step_164;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            decimal num2 = 10;
+            //string num3 = Convert.ToInt32(20);
+
+            //Console.WriteLine(num1Result);
+            //Console.WriteLine(num2Result);
+            //Console.WriteLine(num3Result);
+
+            MathOp FirstOperation = new MathOp();
+            Console.WriteLine(FirstOperation.MathMethod(5));
+            Console.ReadLine();
+
+        }
+    }
+}
+
