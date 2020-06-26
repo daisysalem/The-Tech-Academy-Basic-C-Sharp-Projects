ecb7b55b84b2403f56db130ac9de6efaf2979048
daisysalem <62805119+daisysalem@users.noreply.github.com>
2020-06-26 12:52:57 -0700

    CS - Class-Void-Overload - Step 169

---
+++ CS - Class-Void-Overload - Step 169/CS - Class-Void-Overload - Step 169/MathOp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace CS___Class_Void_Overload___Step_169
+{
+    public class MathOp
+    {
+  
+            public void MathMethod(int num1, int num2)
+            {
+                int result = num1 / 2;
+                int result2 = num2 / 2;
+                Console.WriteLine("Your number divided by 2 = " + result2);
+            }
+                
+           
+                
+        
+    }
+}
---
+++ CS - Class-Void-Overload - Step 169/CS - Class-Void-Overload - Step 169/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+1. Create a class. In that class, create a void method that outputs an integer. 
+Have the method divide the data passed to it by 2.
+
+2. In the Main() method, instantiate that class.
+
+3. Have the user enter a number. Call the method on that number. 
+Display the output to the screen. It should be the entered number, divided by two.
+
+4. Create a method with output parameters.
+
+5. Overload a method.
+
+6. Declare a class to be static.
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS___Class_Void_Overload___Step_169
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            Console.WriteLine("Pick a number.");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            
+
+            MathOp FirstOp = new MathOp();
+            FirstOp.MathMethod(num1, num2);
+
+            Console.ReadLine();
+        }
+    }
+}
