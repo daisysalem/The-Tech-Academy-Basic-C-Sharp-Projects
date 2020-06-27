e9f43553ac456cfd40de154a309f7d3585aa1e1d
daisysalem <62805119+daisysalem@users.noreply.github.com>
2020-06-26 22:33:48 -0700

    CS - Class-Inheritance - Step 174

---
+++ CS - Class-Inheritance - Step 174/CS - Class-Inheritance - Step 174/Employee.cs
@@ -0,0 +1,17 @@
+﻿/*
+3. Create another class called Employee and have it inherit from the Person class. 
+Give the Employee class a property called Id and have it be of data type int.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CS___Class_Inheritance___Step_174
+{
+    public class Employee : Person
+    {
+        public int ID { get; set; }
+    }
+}
---
+++ CS - Class-Inheritance - Step 174/CS - Class-Inheritance - Step 174/Person.cs
@@ -0,0 +1,27 @@
+﻿/*
+1. Create a class called Person and give it two properties, each of data type string. 
+One called FirstName, the other LastName.
+
+2. Give this class a void method called SayName() that takes no parameters and simply
+writes the person's full name to the console in the format of: "Name: [full name]".
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS___Class_Inheritance___Step_174
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()  
+        {
+                Console.WriteLine("Name: " + FirstName + " " + LastName);
+            
+        }
+
+    }
+}
