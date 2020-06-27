69c53037e55643cc7ccf2cdadd86059131fb604e
daisysalem <62805119+daisysalem@users.noreply.github.com>
2020-06-27 14:12:49 -0700

    CS - Abstract Class - Step 179

---
+++ CS - Abstract Class - Step 179/CS - Abstract Class - Step 179/Employee.cs
@@ -0,0 +1,29 @@
+﻿/*
+ 1. Create an interface called IQuittable and have it define a void method called Quit().
+
+2. Have your Employee class from the previous drill inherit that interface and implement 
+the Quit() method in any way you choose.
+
+3. Use polymorphism to create an object of type IQuittable and call the Quit() method on it. 
+Hint: an object can be of an interface type if it implements that specific interface.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS___Abstract_Class___Step_179
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+
+        }
+        public void Quit(Person FirstName, Person LastName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CS - Abstract Class - Step 179/CS - Abstract Class - Step 179/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ 1. Create an abstract class called Person with two properties: string firstName and string lastName.
+
+2. Give it the method SayName().
+
+3. Create another class called Employee and have it inherit from the Person class.
+
+4. Implement the SayName() method inside of the Employee class.
+
+5. Inside the Main() method, instantiate an Employee object with 
+firstName "Sample" and lastName "Student". Call the SayName() method on the object.
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace CS___Abstract_Class___Step_179
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Person = new Employee();
+            Person.FirstName = new string("Sample");
+            Person.LastName = new string("Student");
+            Person.SayName();
+            Console.ReadLine();
+
+            //IQuittable.Resign = "John";
+            //Console.WriteLine(IQuittable.Quit(Resign());
+
+        }
+    }
+}
