8f9b8e0edbdbe162885bd145b30e019baf617666
daisysalem <62805119+daisysalem@users.noreply.github.com>
2020-06-29 21:22:44 -0700

    CS - Struct - Step 200

---
+++ CS - Struct - Step 200/CS - Struct - Step 200/Number.cs
@@ -0,0 +1,21 @@
+﻿/*
+1. Create a struct called Number and give it the property "Amount" and have it be of data type decimal.
+
+2. In the Main() method, create an object of data type Number and assign an amount to it.
+
+3. Print this amount to the console.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace CS___Struct___Step_200
+{
+    public struct Number
+    {
+        public decimal Amount;
+    }
+};
+
---
+++ CS - Struct - Step 200/CS - Struct - Step 200/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+1. Create a struct called Number and give it the property "Amount" and have it be of data type decimal.
+
+2. In the Main() method, create an object of data type Number and assign an amount to it.
+
+3. Print this amount to the console.
+*/
+
+using System;
+
+namespace CS___Struct___Step_200
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number s;
+            s.Amount = 10;
+
+            Console.WriteLine(s.Amount);
+        }
+    }
+}
+
