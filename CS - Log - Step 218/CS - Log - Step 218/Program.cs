82364434ad8861b96beb45774c28cccf717c3a97
daisysalem <62805119+daisysalem@users.noreply.github.com>
2020-07-13 14:11:29 -0700

    CS - Log - Step 218

---
+++ CS - Log - Step 218/CS - Log - Step 218/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+
+1. Ask the user for a number.
+
+2. Log that number to a text file.
+
+3. Print the text file back to the user.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS___Log___Step_218
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in a number");
+            string answer = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Daisy\Documents\Logs\Step218.txt", answer);
+
+            using (StreamReader r = File.OpenText(@"C:\Users\Daisy\Documents\Logs\Step218.txt"))
+            {
+                File.ReadAllText(@"C:\Users\Daisy\Documents\Logs\Step218.txt");
+                Console.WriteLine(File.OpenText(@"C:\Users\Daisy\Documents\Logs\Step218.txt"));
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
