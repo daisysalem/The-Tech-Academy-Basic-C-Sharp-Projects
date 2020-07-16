88809ff93cea2582aae72aa8741a82177d56e369
daisysalem <62805119+daisysalem@users.noreply.github.com>
2020-07-15 18:45:59 -0700

    CS - Const - Step 229

---
+++ CS - Const - Step 229/CS - Const - Step 229/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+
+1. Create a const variable.
+
+2. Create a variable using the keyword "var".
+
+3. Chain two constructors together.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS___Const___Step_229
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string MovieTitle = "Jumanji";
+            var PaidSeat = "Row 5, Seat 3";
+            Console.WriteLine("Enjoy today's feature film, {0}", MovieTitle);
+            Console.WriteLine("You seat is " + PaidSeat);
+            Console.ReadLine();
+     
+        }
+    }
+}
+
---
+++ CS - Const - Step 229/CS - Const - Step 229/SeatChart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS___Const___Step_229
+{
+    public class SeatChart
+    {
+        public SeatChart(string PaidSeat) : this(PaidSeat, 10)
+        {
+
+        }
+
+        public SeatChart(string PaidSeat, int Payment)
+        {
+            string CustSeating = PaidSeat;
+            int Price = Payment;
+            
+        }
+       
+    }
+}
+
